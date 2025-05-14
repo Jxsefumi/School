d69bfd1fa56c9fb9b18ae6714aae339965e8fd38
Jxsefumi <josefumi89@gmail.com>
2025-05-14 23:06:50 +0800

    Delete OOP Finals directory

--- OOP Finals/connect.php
+++
@@ -1,8 +0,0 @@
-<?php
-$con = mysqli_connect("localhost", "Order", "Order", "Order");
-
-if(!$con){
-    die("Connection Failed". mysqli_connect_error());
-}
-
-?>
--- OOP Finals/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-require('connect.php');
-session_start();
-$_SESSION = [];
-session_unset();
-session_destroy();
-header("Location:index.php")
-?>
